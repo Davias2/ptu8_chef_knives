3905483c3d8eaaf05a753aff5dc4ce89311ee719
Dovydas Žalys <doviduse@gmail.com>
2023-03-15 22:52:58 +0200

    modelis

--- ptu8_chef_knives/chef_knives/models.py
+++ ptu8_chef_knives/chef_knives/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    stock = models.IntegerField()
+    image_url = models.CharField(max_length=350)
+
+    def __str__(self) -> str:
+        return self.name
