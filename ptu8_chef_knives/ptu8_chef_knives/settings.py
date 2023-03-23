382fd543a6d408446038fededd140d788e63f601
Dovydas Žalys <doviduse@gmail.com>
2023-03-23 19:11:35 +0200

    password reset

--- ptu8_chef_knives/ptu8_chef_knives/settings.py
+++ ptu8_chef_knives/ptu8_chef_knives/settings.py
@@ -129,3 +129,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_POST = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = local_settings.EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = local_settings.EMAIL_HOST_PASSWORD
